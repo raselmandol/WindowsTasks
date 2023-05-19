3fe19959a565c4052da88320a277903cf7dc3755
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-05-19 22:39:56 +0530

    Create ExampleDeleteCrawl.c

---
+++ Registry/ExampleDeleteCrawl.c
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+int main()
+{
+    //The path of the registry key to be deleted
+    const char* keyPath = "SOFTWARE\\MyApplication";
+    LONG result = RegDeleteKeyA(HKEY_CURRENT_USER, keyPath);
+    if (result == ERROR_SUCCESS)
+    {
+        printf("Registry key deleted successfully.\n");
+    }
+    else
+    {
+        printf("Failed to delete registry key. Error code: %ld\n", result);
+    }
+
+    return 0;
+}
