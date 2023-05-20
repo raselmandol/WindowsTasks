59d75773f858d5ec5ee08063a90414ad59cb981b
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-05-20 22:45:32 +0530

    Create StopAService.c

---
+++ Services/StopAService.c
@@ -0,0 +1,35 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+    SC_HANDLE hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
+    if (hSCManager == NULL)
+    {
+        printf("Failed to open service control manager.\n");
+        return 1;
+    }
+
+    SC_HANDLE hService = OpenService(hSCManager, "MyServiceName", SERVICE_STOP);
+    if (hService == NULL)
+    {
+        printf("Failed to open service.\n");
+        CloseServiceHandle(hSCManager);
+        return 1;
+    }
+
+    SERVICE_STATUS serviceStatus;
+    if (ControlService(hService, SERVICE_CONTROL_STOP, &serviceStatus))
+    {
+        printf("Service stopped successfully.\n");
+    }
+    else
+    {
+        printf("Failed to stop the service.\n");
+    }
+
+    CloseServiceHandle(hService);
+    CloseServiceHandle(hSCManager);
+
+    return 0;
+}
