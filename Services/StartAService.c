ca3271536fc7946121d0fc4cf2751cd2f346c3c5
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-05-20 22:49:05 +0530

    Create StartAService.c

---
+++ Services/StartAService.c
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+    SC_HANDLE hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
+    if (hSCManager == NULL)
+    {
+        printf("Failed to open service control manager.\n");
+        return 1;
+    }
+
+    SC_HANDLE hService = OpenService(hSCManager, "MyServiceName", SERVICE_START);
+    if (hService == NULL)
+    {
+        printf("Failed to open service.\n");
+        CloseServiceHandle(hSCManager);
+        return 1;
+    }
+
+    if (StartService(hService, 0, NULL))
+    {
+        printf("Service started successfully.\n");
+    }
+    else
+    {
+        printf("Failed to start the service.\n");
+    }
+
+    CloseServiceHandle(hService);
+    CloseServiceHandle(hSCManager);
+
+    return 0;
+}
