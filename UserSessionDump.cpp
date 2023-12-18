9033f7435eb5f1806ae2f0250809a054f30e06b1
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-12-18 20:30:07 +0530

    Create UserSessionDump.cpp

---
+++ UserSessionDump.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Windows.h>
+#include <Wtsapi32.h>
+
+#pragma comment(lib, "Wtsapi32.lib")
+
+int main() {
+    DWORD sessionId = WTSGetActiveConsoleSessionId();
+    WCHAR* username = nullptr;
+    DWORD size;
+    if (WTSQuerySessionInformationW(WTS_CURRENT_SERVER_HANDLE, sessionId, WTSUserName, &username, &size)) {
+        std::wcout << "Session ID: " << sessionId << std::endl;
+        std::wcout << "User for Session ID " << sessionId << ": " << username << std::endl;
+
+        WTSFreeMemory(username);
+    } else {
+        std::cerr << "Failed to get session information. Error code: " << GetLastError() << std::endl;
+    }
+
+    return 0;
+}
