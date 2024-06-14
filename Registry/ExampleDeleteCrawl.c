9b3522b6dc19c49226dbe691bfe7969082abff58
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-06-14 23:41:28 +0600

    Update ExampleDeleteCrawl.c

--- Registry/ExampleDeleteCrawl.c
+++ Registry/ExampleDeleteCrawl.c
@@ -3,7 +3,8 @@
 int main()
 {
     //The path of the registry key to be deleted
-    const char* keyPath = "SOFTWARE\\MyApplication";
+    const char* keyPath = "SOFTWARE\\MyApplication"; //adjust this, may change
+    // you can add this to vector, list
     LONG result = RegDeleteKeyA(HKEY_CURRENT_USER, keyPath);
     if (result == ERROR_SUCCESS)
     {
