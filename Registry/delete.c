e79ed464495bb50c5eb8d08f01b5932ab5903377
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-07-08 23:25:23 +0600

    Update delete.c

--- Registry/delete.c
+++ Registry/delete.c
@@ -2,16 +2,18 @@
 #include <windows.h>
 
 int main() {
+    // define path
     LPCSTR keyPath = "SOFTWARE\\software\\Settings";
 
     HKEY hKey;
     LONG regOpenResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE, keyPath, 0, KEY_ALL_ACCESS, &hKey);
 
     if (regOpenResult != ERROR_SUCCESS) {
-        printf("error opening registry key. error code: %ld\n", regOpenResult);
+        printf("error opening registry key. error code: %ld\n", regOpenResult); //check
         return 1;
     }
 
+    //delete
     LONG regDeleteResult = RegDeleteTree(hKey, NULL);
 
     if (regDeleteResult == ERROR_SUCCESS) {
